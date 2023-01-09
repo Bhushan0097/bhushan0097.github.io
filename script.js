334169562ab320bfc2148164a0b507dad0f2f516
DESKTOP-SQSR5SR\Asus 1 <bhushannagrale21@outlook.com>
2023-01-09 23:22:00 +0530

    updated layout

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",(
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
